95b77390730d8f0c2fee2f11558d484df2349e3a
Adrian Rangel <adrianrangelargu@gmail.com>
2026-02-02 19:29:54 -0400

    feat: added markdown rendering with syntax highlighting and GFM support

--- src/features/preview/Preview.tsx
+++ src/features/preview/Preview.tsx
@@ -1,9 +1,25 @@
+import rehypeStarryNight from "rehype-starry-night";
+import rehypeRaw from "rehype-raw";
+import rehypeSanitize from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
+import { MarkdownHooks } from "react-markdown";
+import { useEditorStore } from "@/shared/model/store";
+
 export const Preview = () => {
+  const { documentContent } = useEditorStore();
+
   return (
     <section className="w-full flex flex-col">
       <div className="w-full flex items-center px-4 py-2 bg-gray-100 border border-gray-200">
         <h3 className="font-semibold text-gray-600 uppercase">Preview</h3>
       </div>
+      <div className="prose">
+        <MarkdownHooks
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeStarryNight, rehypeRaw, rehypeSanitize]}
+          children={documentContent}
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
