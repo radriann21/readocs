f8b9458557374104b2b7a7abd550fcdd0d3ce05f
Adrian Rangel <adrianrangelargu@gmail.com>
2026-02-05 21:53:27 -0400

    feat: improved editor and preview layout with fixed heights and scrolling

--- src/features/editor/components/CodeMirrorEditor.tsx
+++ src/features/editor/components/CodeMirrorEditor.tsx
@@ -1,20 +1,25 @@
-import CodeMirror from "@uiw/react-codemirror"
-import { markdown, markdownLanguage } from "@codemirror/lang-markdown"
-import { languages } from "@codemirror/language-data"
-import { useEditorStore } from "@/shared/model/store"
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { languages } from "@codemirror/language-data";
+import { useEditorStore } from "@/shared/model/store";
 
 export const CodeMirrorEditor = () => {
   const { documentContent, setDocumentContent } = useEditorStore();
 
   return (
-    <CodeMirror
-      style={{
-        height: "100%",
-        width: "100%",
-      }}
-      value={documentContent}
-      onChange={setDocumentContent}
-      extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]}
-    />
+    <div className="w-full h-full">
+      <CodeMirror
+        style={{
+          height: "calc(100vh - 12rem)",
+          width: "100%",
+          overflow: "scroll",
+        }}
+        value={documentContent}
+        onChange={setDocumentContent}
+        extensions={[
+          markdown({ base: markdownLanguage, codeLanguages: languages }),
+        ]}
+      />
+    </div>
   )
 }
