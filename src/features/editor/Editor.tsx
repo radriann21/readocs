995333acbf7a92f75d38d510d8e5298b3c31a808
Adrian Rangel <adrianrangelargu@gmail.com>
2026-02-06 20:48:59 -0400

    feat: extracted editor header into separate EditorHeader component

--- src/features/editor/Editor.tsx
+++ src/features/editor/Editor.tsx
@@ -1,14 +1,13 @@
 import { Toolbar } from "./components/Toolbar";
 import { CodeMirrorEditor } from "./components/CodeMirrorEditor";
+import { EditorHeader } from "./components/EditorHeader";
 
 export const Editor = () => {
   return (
     <section className="w-full flex flex-col">
-      <div className="flex items-center px-4 py-2 bg-gray-100 border border-gray-200">
-        <h3 className="font-semibold text-gray-600 uppercase">Editor</h3>
-      </div>
-     <Toolbar />
-     <CodeMirrorEditor />
+      <EditorHeader />
+      <Toolbar />
+      <CodeMirrorEditor />
     </section>
   );
 };
